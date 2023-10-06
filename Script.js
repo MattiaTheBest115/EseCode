4dad556ad59e290093c5fc0d65d6bcfc58b2727c
Esentasse <65710440+MattiaTheBest115@users.noreply.github.com>
2023-10-06 12:32:21 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,138 @@
+﻿function ricaricaPagina() {
+    location.reload();
+}
+
+function formatNumber(input) {
+    var value = input.value.replace(/[,.]/g, '');
+    if (value === '' || isNaN(value)) {
+        return;
+    }
+    var formattedValue = parseFloat(value).toLocaleString('it-IT');
+    input.value = formattedValue;
+}
+
+function validateInput(inputElement) {
+    var inputValue = inputElement.value;
+
+    inputValue = inputValue.replace(/[^0-9]/g, '');
+
+    var min = parseInt(inputElement.min);
+    var max = parseInt(inputElement.max);
+    var numericValue = parseInt(inputValue);
+
+    if (isNaN(numericValue)) {
+        inputElement.value = '';
+    } else if (numericValue < min) {
+        inputElement.value = min;
+    } else if (numericValue > max) {
+        inputElement.value = max;
+    } else {
+        inputElement.value = numericValue;
+    }
+}
+
+function checkLevel1(input) {
+    var value = parseInt(input.value, 10);
+    if (isNaN(value)) {
+        input.value = 0;
+    } else if (value < 0) {
+        input.value = 0;
+    } else if (value > 255) {
+        input.value = 255;
+    }
+}
+
+function changeBackgroundColor(select) {
+    var selectedValue = select.options[select.selectedIndex].value;
+    if (selectedValue === "♂") {
+        select.style.backgroundColor = "blue";
+        select.style.color = "white";
+    } else if (selectedValue === "♀") {
+        select.style.backgroundColor = "red";
+        select.style.color = "white";
+    } else {
+        select.style.backgroundColor = "";
+        select.style.color = "";
+    }
+}
+
+function capitalizeInput(input) {
+    const inputValue = input.value;
+    const capitalizedValue = inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
+    input.value = capitalizedValue;
+}
+document.addEventListener("DOMContentLoaded", function () {
+
+    const copyButton = document.getElementById("copyButton");
+    const form = document.getElementById("pokemon-form");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const NomeAllenatore = document.getElementById("NomeAllenatore").value;
+        const Sprite = document.getElementById("Sprite").value;
+        const ClasseAllenatore = document.getElementById("ClasseAllenatore").value;
+        const TotalePokemon = document.getElementById("TotalePokemon").value;
+        const Colore1 = document.getElementById("Colore1").value;
+        const Colore2 = document.getElementById("Colore2").value;
+        const Ricompensa = document.getElementById("Ricompensa").value;
+        const Gioco = document.getElementById("Gioco").value;
+        const Posizione = document.getElementById("posizione").value;
+
+        // NON CAMBIARE FORMATTAZIONE
+        const wikicode = `{{Squadra
+|type=${Colore1}
+|type2=${Colore2}
+|pokemon=${TotalePokemon}
+|sprite=${Sprite}
+|prize=${Ricompensa} {{PDollar}}
+|class=${ClasseAllenatore}
+|name=${NomeAllenatore}
+|game=${Gioco}
+|location=[[${Posizione}]]
+`;
+        document.getElementById("wikicode-result").textContent = wikicode;
+
+        copyButton.addEventListener("click", function copyToClipboard() {
+            const tempInput = document.createElement("textarea");
+            tempInput.value = wikicode;
+            document.body.appendChild(tempInput);
+
+            tempInput.select();
+            tempInput.setSelectionRange(0, 99999);
+
+            document.execCommand("copy");
+            document.body.removeChild(tempInput);
+
+            copyButton.textContent = "Copiato!";
+            setTimeout(function () {
+                copyButton.textContent = "Copia";
+            }, 2000);
+
+            copyButton.removeEventListener("click", copyToClipboard);
+        });
+    });
+});
+
+function traduciNomePokemon(inputElement) {
+    var inputText = inputElement.value.toLowerCase();
+    var translatedText = traduzioniNomiPokemon[inputText] || inputElement.value;
+    inputElement.value = translatedText;
+
+    if (translatedText !== inputElement.value) {
+        document.getElementById('VecchiaParola').value = inputElement.value;
+    }
+
+    inputElement.value = translatedText;
+}
+
+function traduciElemento(inputElement, dizionario) {
+    var inputText = inputElement.value.toLowerCase();
+
+    var translatedText = dizionario[inputText] || inputElement.value;
+
+    if (translatedText !== inputElement.value) {
+        document.getElementById('VecchiaParola').value = inputElement.value;
+        inputElement.value = translatedText;
+    }
+}
